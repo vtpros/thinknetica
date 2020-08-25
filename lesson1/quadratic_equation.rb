ae392b8516a1c587b359c739ea1e3edf8c2c83f5
Vladimir Trachenko <trachenko@gmail.com>
2020-08-25 08:46:57 +0300

    Refactor to follow ruby style guide

--- lesson1/quadratic_equation.rb
+++ lesson1/quadratic_equation.rb
@@ -14,8 +14,8 @@ if discriminant < 0
 end
 
 d_sqrt = Math::sqrt( discriminant )
-x1 = (-b + d_sqrt) / 2*a
-x2 = (-b - d_sqrt) / 2*a
+x1 = (-b + d_sqrt)/2*a
+x2 = (-b - d_sqrt)/2*a
 
 if discriminant > 0
   puts "Discriminant: #{discriminant}, x1: #{x1}, x2: #{x2}"
